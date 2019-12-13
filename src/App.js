d4ab868d6e1d0f4780515af05da0c95f4d01ed27
ana-sanchez <snchz.rojo@gmail.com>
2019-12-13 18:42:40 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
+import Header from './components/header'
+
+
 import './App.scss';
+
+
 
 class App extends Component {
   render() {
-    const header = (
-      <div className="header-card">
-        <img className="img" alt="mono"></img>
-
-        <div className="name-card">
-          <h2 className="name-title">Alex Guerrero</h2>
-          <h4 className="name-date">Lunes, 26 de junio de 2017</h4>
-        </div>
-      </div>
-
+    return (
+      <Header></Header>
     )
-    return header
   }
 
 }
