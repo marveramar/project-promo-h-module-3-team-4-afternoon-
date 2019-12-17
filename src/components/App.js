1e0310b3bf406b1e8e4903b1038b8605faf28407
ana-sanchez <snchz.rojo@gmail.com>
2019-12-17 18:51:31 +0100

    add react route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import '../styles/App.scss';
-
 import React from 'react';
-import Header from './Header';
-import Palette from './PaletteDesign';
-import Fonts from './FontsDesign';
-import CardPreview from './CardPreview';
-import SharedForm from './SharedForm';
-import Input from './InputForm';
-import AppFooter from './Footer';
+import { Route, Switch } from 'react-router-dom'
+import Landing from './Landing';
+import Home from './Home';
+
+
+// import Header from './Header';
+// import Palette from './PaletteDesign';
+// import Fonts from './FontsDesign';
+// import CardPreview from './CardPreview';
+// import SharedForm from './SharedForm';
+// import Input from './InputForm';
+// import AppFooter from './Footer';
 
 //import LandingHome from'./components/landing';
 
@@ -15,14 +19,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="Main">
-        {/*<LandingHome></LandingHome>*/}
-        <Header></Header>
-        <CardPreview></CardPreview>
-        <Palette></Palette>
-        <Fonts></Fonts>
-        <Input></Input>
-        <SharedForm></SharedForm>
-        <AppFooter></AppFooter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/Home" component={Home} />
+        </Switch>
       </div >
     )
   }
