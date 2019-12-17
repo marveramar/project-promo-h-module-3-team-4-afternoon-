3a1968692a2c92228fb9f0c690fadfac89f9e407
ana-sanchez <snchz.rojo@gmail.com>
2019-12-17 20:15:44 +0100

    fix footer mediaquerys

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Home from './Home';
 class App extends React.Component {
   render() {
     return (
-      <div className="Main">
+      <div className="App">
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route exact path="/Home" component={Home} />
