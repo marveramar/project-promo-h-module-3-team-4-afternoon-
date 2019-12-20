345848ca0b9df99ec92b2ea49b9b732ef975c37e
marvermar <m.veramartin@gmail.com>
2019-12-20 10:22:01 +0100

    try to get a function to rotate arrows

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -11,38 +11,39 @@ class Collapsable extends React.Component {
         this.state = {
             styling: ''
         };
-        this.rotateArrow = this.rotateArrow.bind(this);
+        // this.rotateArrow = this.rotateArrow.bind(this);
     }
 
-    rotateArrow(event) {
-        this.setState((prevState, props) => {
-            let nextStyling;
-            if (prevState.styling === '') {
-                nextStyling = 'rotate';
-            } else {
-                nextStyling = '';
-            }
-            return {
-                styling: nextStyling
-            }
-        });
-    }
+    // rotateArrow(event) {
+    //     this.setState((prevState, props) => {
+    //         let nextStyling;
+    //         if (prevState.styling === '') {
+    //             nextStyling = 'rotate';
+    //         } else {
+    //             nextStyling = '';
+    //         }
+    //         return {
+    //             styling: nextStyling
+    //         }
+    //     });
+    // }
+
     render() {
         return (
             <fieldset className="fieldset fieldset__design">
                 <div className="form_title-container">
                     <form action="" method="POST" id="card__form">
                         <Accordion>
-                            {[<div  onClick={this.rotateArrow} id="designContainer" className="form_title-container">
+                            {[<div id="designContainer" className="form_title-container">
                                 <legend className="form_title"><i className="far fa-object-ungroup form_title-icon"></i>DISEÑA</legend>
-                                <span className="arrowDesign" id="arrowDesign"><i className={`fas fa-chevron-down ${this.state.styling}`}></i></span>
+                                <span className="arrowDesign" id="arrowDesign"><i className={"fas fa-chevron-down "}></i></span>
                             </div>, <div id="fillInContainer" className="form_title-container">
                                 <legend className="form_title"><i className="far fa-keyboard form_title-icon"></i>RELLENA</legend>
                                 <span id="arrowFillIn"><i className="fas fa-chevron-down"></i></span>
                             </div>, <div id="shareContainer" className="form_title-container">
                                 <legend className="form_title"><i className="fas fa-share-alt form_title-icon"></i>COMPARTE</legend>
                                 <span id="arrowShare"><i className="fas fa-chevron-down"></i></span>
-                            </div>].map((item, index) => {
+                            </div>].map((item, index, id) => {
 
                                 return (<AccordionItem
                                     key={index}
@@ -50,15 +51,9 @@ class Collapsable extends React.Component {
                                     id={index}
                                 >
                                     <div ref="itemRef">
-                                        {index === 0 ? (
-                                            <Palette />
-                                        ) : null}
-                                        {index === 1 ? (
-                                            <Input />
-                                        ) : null}
-                                        {index === 2 ? (
-                                            <SharedForm />
-                                        ) : null}
+                                        {index === 0 ? (<Palette />) : null}
+                                        {index === 1 ? (<Input />) : null}
+                                        {index === 2 ? (<SharedForm />) : null}
                                     </div>
                                 </AccordionItem>
                                 );
